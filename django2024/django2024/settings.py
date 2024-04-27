4a2118ed110660b7406c20e3375bd94a7ce600ae
mohammed alsamaki <samkimohd@gmail.com>
2024-04-27 10:46:51 +0300

    add register

--- django2024/django2024/settings.py
+++ django2024/django2024/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%dl4eyk#ue$tx!9-w_fw$zz@lfbr$ehgp13e_wa%i-g$i9d@i*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
